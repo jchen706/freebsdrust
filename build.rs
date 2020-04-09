148814788b351edcce75c54aaf1bd86158ec60df
Jun <jchen706@gatech.edu>
2020-04-08 21:27:56 -0400

    d

--- build.rs
+++ build.rs
@@ -8,7 +8,7 @@ use std::path::PathBuf;
 
 
 fn main() {
-    env::set_var("LIBCLANG_PATH", "/usr/local/llvm80/lib");
+    //env::set_var("LIBCLANG_PATH", "/usr/local/llvm80/lib");
 
     // Tell cargo to tell rustc to link the system bzip2
     // shared library.
